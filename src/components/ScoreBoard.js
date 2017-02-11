7995d9d3f4aed076fce1c003e4a53edad7ea828a
Marian <marianhalliday@gmail.com>
2017-02-10 20:31:16 -0500

    Added facebook share button

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import {
+  ShareButtons,
+  generateShareIcon
+} from 'react-share';
 
 class ScoreBoard extends React.Component {
   render() {
+    const {
+      FacebookShareButton
+    } = ShareButtons;
+
+    const FacebookIcon = generateShareIcon('facebook');
+
     return(
       <div className="scoreBoard">
         <p className="player">
@@ -10,6 +20,13 @@ class ScoreBoard extends React.Component {
         <p className="player">
           {this.props.player2Name}: {this.props.player2Wins}
         </p>
+        <FacebookShareButton
+          url='https://xoxoreactgame.herokuapp.com/'
+          title='xoxo Game'
+          description ='Play a game you will never forget' picture='https://xoxoreactgame.herokuapp.com/xoxo-img.png'
+        >
+          <FacebookIcon size={32} />
+        </FacebookShareButton>
       </div>
     )
   }
